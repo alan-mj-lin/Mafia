3210eb723685c4e8d57cb60bbfd40a888d20e35a
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-05-01 17:04:29 -0400

    changing host and port to match aws

--- main.py
+++ main.py
@@ -347,7 +347,7 @@ def disable_hang():
 if __name__ == '__main__':
     socketio.run(
         app, 
-        host='127.0.0.1', 
-        port=8000, 
+        host='0.0.0.0', 
+        port=9000, 
         debug=True
     )
