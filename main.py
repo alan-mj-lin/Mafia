321cd3f652cb875a93e7c5ab452d7990538cdd96
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-05-01 16:55:30 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,353 @@
+#!/usr/bin/env python3.7
+# Sporeas 1.1.0
+"""
+This is the main file to run.
+"""
+
+# pylint: disable=invalid-name
+
+# Need to monkey patch eventlet to prevent hang
+#import gevent.monkey; gevent.monkey.patch_all()
+import json
+import random, string
+import collections
+import eventlet
+eventlet.monkey_patch(socket=False)
+import requests
+from flask import Flask, url_for, render_template, request, redirect, jsonify
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from player import Player
+
+app = Flask(__name__)
+socketio = SocketIO(app,
+                    manage_session=False,
+                    logger=True
+                    )
+
+gamekey = 'ffffffff'
+numMafia = 0
+players = []
+roles = []
+SERVER_URL = 'http://127.0.0.1:8000/'
+activity = {
+        'target': '',
+        'save': ''
+    }
+
+def generateGameRoomKey(length=8):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def win_check():
+    dead_mafia = 0
+    civilians = 0
+    sid = players[0].sid
+    for i in players:
+        if i.role == 'mafia' and i.status == 'dead':
+            dead_mafia += 1
+
+    if dead_mafia == numMafia:
+        emit('result', {"winners": 'town'}, room=sid)
+
+    # check for living people in the town
+    for i in players:
+        if i.role != 'mafia' and i.status != 'dead':
+            civilians +=1
+
+    if civilians <= numMafia - dead_mafia:
+        print(civilians)
+        print(numMafia - dead_mafia)
+        emit('result', {"winners": 'mafia'}, room=sid)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def process():
+    global numMafia, gamekey
+    numMafia = int(request.form['mafia'])
+    #json = str(temp['mafia'].value)
+    #numMafia = int(temp)
+    print(numMafia)
+    gamekey = generateGameRoomKey()
+    return jsonify({'redirect': gamekey})
+
+@app.route('/getkey', methods=['GET'])
+def getkey():
+    
+    return redirect(SERVER_URL + gamekey)
+
+
+@app.route('/<key>', methods=['GET', 'POST'])
+def gameroom(key):
+    """
+    Serve the game room
+    """
+    global gamekey, numMafia
+    if key == gamekey and key != 'ffffffff':
+        print(numMafia)
+        return render_template("gameroom.html")
+
+
+@socketio.on('connect')
+def connect_test():
+    """
+    Get player name
+    """
+    emit('get name')
+
+@socketio.on('observe')
+def observe():
+    join_room('watcher')
+
+@socketio.on('message')
+def message():
+    global gamekey
+    emit('update log', room=gamekey)
+    emit('update watcher log', room='watcher')
+
+@socketio.on('watcher message')
+def watcher_message():
+    emit('update watcher log', room='watcher')
+
+@socketio.on('add player')
+def add_player(message):
+    global gamekey
+    sid = request.sid
+    print(sid)
+    player = Player(message['name'], sid)
+    players.append(player)
+    join_room(gamekey)
+    emit('update', room=gamekey)
+    emit('update', room='watcher')
+    emit('add event listeners', {"name": player.name}, room=gamekey)
+
+@socketio.on('clear')
+def clear():
+    global gamekey, players, numMafia, roles
+    gamekey = 'ffffffff'
+    players = []
+    numMafia = 0
+    roles = []
+
+@socketio.on('shuffle')
+def shuffle():
+    global players, numMafia
+    numPlayers = len(players)
+    # killers
+    for i in range(0, numMafia):
+        roles.append('mafia')
+    # doctor
+    roles.append('doctor')
+    
+    # detective
+    roles.append('detective')
+
+    # the rest
+    civilians = numPlayers - numMafia - 2
+    for i in range(0, civilians):
+        roles.append('civilian')
+
+    random.shuffle(roles)
+    print(roles)
+    print(players)
+    count = 0
+    for i in players:
+        name = i.name
+        sid = i.sid
+        i.role = roles[count]
+        role = i.role
+        count+= 1
+        print(name, sid, role)
+        emit('assign', {name : role}, room=sid)
+        emit('assign', {name : role}, room='watcher')
+
+    for i in players:
+        name = i.name
+        if i.role == 'mafia':
+            for j in players:
+                if j.role =='mafia':
+                    emit('assign', {name : i.role}, room=j.sid)
+
+@socketio.on('show screen')
+def show_screen():
+    emit('show', room=gamekey)
+
+@socketio.on('disable start')
+def disable_start():
+    emit('prevent start', room=gamekey)
+    emit('prevent start', room='watcher')
+
+@socketio.on('disable')
+def disable():
+    emit('disable all', room=gamekey)
+    emit('disable all', room='watcher')
+
+@socketio.on('kill phase')
+def kill_phase():
+    global players
+    for i in players:
+        if i.role == 'mafia':
+            emit('kill enable', room=i.sid)
+
+@socketio.on('kill')
+def kill(message):
+    global activity
+    activity['target'] = message['name']
+
+@socketio.on('hang')
+def hang_and_win_check(message):
+    target = message['target']
+    for i in players:
+        if i.name == target:
+            i.status = 'dead'
+    emit('night over', {"deaths": target})
+
+    win_check()
+
+
+@socketio.on('kill check')
+def kill_check(message):
+    target = message['target']
+    for i in players:
+        if i.name == target and i.status=='active':
+            emit('enter kill phase', {"name": i.name}, room=gamekey)
+
+@socketio.on('save phase')
+def save_phase():
+    global players
+    for i in players:
+        if i.role == 'doctor':
+            emit('save enable', room=i.sid)
+
+@socketio.on('save')
+def save(message):
+    global activity
+    for i in players:
+        if i.role =='doctor' and i.status == 'active':
+            activity['save'] = message['name']
+        else:
+            activity['save'] = ''
+
+@socketio.on('save check')
+def save_check(message):
+    target = message['target']
+    doctor_alive = True
+    for i in players:
+        if i.role =='doctor' and i.status == 'dead':
+            doctor_alive = False
+    
+    if doctor_alive:
+        for i in players:
+            if i.name == target and i.status=='active':
+                emit('enter save phase', {"name": i.name}, room=gamekey)
+    else:
+        emit('enter save phase', {"name": ''}, room=gamekey)
+
+@socketio.on('detect phase')
+def detect_phase():
+    global players
+    for i in players:
+        if i.role == 'detective':
+            emit('detect enable', room=i.sid)
+            break
+
+@socketio.on('detect')
+def detect(message):
+    global players
+    role = ''
+    person_to_check = message['name']
+    for i in players:
+        if i.name == person_to_check:
+            role = i.role
+            break
+
+    for i in players:
+        if i.role == 'detective' and i.status=='active':
+            emit('reveal', {
+                "name": person_to_check,
+                "role": role
+                }, 
+                room=i.sid)
+            break
+
+@socketio.on('detect check')
+def detect_check(message):
+    target = message['target']
+    detective_alive = True
+    for i in players:
+        if i.role == 'detective' and i.status=='dead':
+            detective_alive = False
+    
+    if detective_alive:
+        for i in players:
+            if i.name == target and i.status=='active' and i.role != 'detective':
+                emit('enter detect phase', {"name": i.name}, room=gamekey)
+    else:
+        emit('enter detect phase', {"name": ''}, room=gamekey)
+
+@socketio.on('vote phase')
+def open_season():
+    global gamekey
+    saved = activity['save']
+    killed = activity['target']
+    death = ''
+    for i in players:
+        if i.name == killed and saved != killed:
+            i.status = 'dead'
+            death = i.name
+        elif i.name == killed and saved == killed:
+            pass
+
+    emit('night over', {"deaths": death}, room=gamekey)
+    emit('night over', {"deaths": death}, room='watcher')
+
+    for i in players:
+        if i.status == 'dead':
+            for j in players:
+                emit('reveal actual', {
+                    "name": j.name,
+                    "role": j.role
+                    }, 
+                    room=i.sid)
+
+@socketio.on('evaluate')
+def evaluate():
+    saved = activity['save']
+    killed = activity['target']
+    sid = players[0].sid
+    death = ''
+    for i in players:
+        if i.name == killed and saved != killed:
+            i.status = 'dead'
+            death = i.name
+        elif i.name == killed and saved == killed:
+            pass
+    emit('death message', {"deaths": death}, room=sid)
+    win_check()
+
+@socketio.on('enable night')
+def enable_night():
+    emit('hang enable', room='watcher')
+
+@socketio.on('increment')
+def increment(message):
+    count = int(message['night'])
+    count += 1
+    emit('add count', {"night": count}, room=gamekey)
+    emit('add count', {"night": count}, room='watcher')
+
+@socketio.on('disable hang')
+def disable_hang():
+    emit('hang disable', room='watcher')
+
+if __name__ == '__main__':
+    socketio.run(
+        app, 
+        host='127.0.0.1', 
+        port=8000, 
+        debug=True
+    )
---
+++ static/gameroom.js
@@ -0,0 +1,344 @@
+function appendToStorage(name, data){
+    var old = localStorage.getItem(name);
+    if(old === null) old = "";
+    localStorage.setItem(name, old + data);
+}
+
+function addMessage(title, subtitle) {
+    appendToStorage('log', '<div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3 bg-light">\
+                                <div class="media">\
+                                    <img class="mr-3" src="static/spyicon.jpg" alt="Generic placeholder image">\
+                                    <div class="media-body">\
+                                    <h5 class="mt-0">' + title+ '</h5>'
+                                     + subtitle + '\
+                                    </div>\
+                                </div>\
+                            </div>')
+}
+
+function addWatcherMessage(title, subtitle) {
+    appendToStorage('watcher', '<div class="overflow-auto p-3 mb-3 mb-md-0 mr-md-3 bg-light">\
+                                <div class="media">\
+                                    <img class="mr-3" src="static/spyicon.jpg" alt="Generic placeholder image">\
+                                    <div class="media-body">\
+                                    <h5 class="mt-0">' + title+ '</h5>'
+                                     + subtitle + '\
+                                    </div>\
+                                </div>\
+                            </div>')
+}
+
+//game progression functions
+function nightProgression(socket) {
+    socket.emit('disable');
+    const nightString = count.toString();
+    var screen = localStorage.getItem('screen');
+    $('#screenTitle').html('Night' + nightString);
+    $('#screenSub').html('Mafia pick someone to kill..');
+    localStorage.setItem('screen', $('#screen').html());
+    socket.emit('show screen');
+    socket.emit('kill phase');
+}
+
+function killPhase(target, socket) {
+    socket.emit('disable');
+    document.getElementsByName('kill').disabled = true;
+    socket.emit('kill', {name: target});
+    const nightString = count.toString();
+    $('#screenTitle').html('Night' + nightString);
+    $('#screenSub').html('Doctor pick someone to save..');
+    localStorage.setItem('screen', $('#screen').html());
+    socket.emit('show screen');
+    socket.emit('save phase');
+}
+
+function savePhase(target, socket) {
+    socket.emit('disable')
+    document.getElementsByName('save').disabled = true;
+    socket.emit('save', {name: target});
+    const nightString = count.toString();
+    $('#screenTitle').html('Night' + nightString);
+    $('#screenSub').html('Detective pick someone to interrogate..');
+    localStorage.setItem('screen', $('#screen').html());
+    socket.emit('show screen');
+    socket.emit('detect phase');
+}
+
+function detectPhase(target, socket) { 
+    socket.emit('disable')
+    document.getElementsByName('detect').disabled = true;
+    socket.emit('detect', {name: target});
+    $('#screenTitle').html('Morning');
+    $('#screenSub').html('The night has ended');
+    localStorage.setItem('screen', $('#screen').html());
+    socket.emit('show screen');
+    socket.emit('vote phase');
+    socket.emit('message');
+}
+
+var count = 1;
+
+$(document).ready(function() {
+    const socket = io.connect('http://' + document.domain + ':' + location.port);
+    $('.alert').hide();
+    var board = localStorage.getItem('board');
+    var log = localStorage.getItem('log');
+    if (log) {
+        $('#log').html(log);
+    }
+    if (board){
+        $('#cards').html(board);
+    }
+
+    socket.on('show', function() {
+        var updated = localStorage.getItem('screen');
+        $('#screen').html(updated);
+        $('#screen').modal('show');
+    });
+
+    socket.on('get name', function(){
+        $('#modal').modal('show');
+    });
+
+    socket.on('update', function(){
+        var updated = localStorage.getItem('board');
+        $('#cards').html(updated);
+    });
+
+    socket.on('update log', function(){
+        var updated = localStorage.getItem('log');
+        $('#log').html(updated);
+    });
+
+    socket.on('update watcher log', function(){
+        var updated = localStorage.getItem('watcher');
+        $('#log').html(updated);
+    });
+
+    socket.on('assign', function(message){
+        const name = Object.keys(message)[0];
+        const role = Object.values(message)[0];
+        $('#'+ name+'_role').html(role);
+    });
+
+    socket.on('disable all', function(){
+        $('a[name="kill"]').addClass('disabled');
+        $('a[name="save"]').addClass('disabled');
+        $('a[name="detect"]').addClass('disabled');
+        $('a[name="hang"]').addClass('disabled');
+        document.getElementById("night").disabled = true;
+    });
+
+    socket.on('kill enable', function(){
+        $('a[name="kill"]').removeClass('disabled');
+    });
+
+    socket.on('save enable', function(){
+        $('a[name="save"]').removeClass('disabled');
+    });
+
+    socket.on('detect enable', function(){
+        $('a[name="detect"]').removeClass('disabled');
+    });
+
+    socket.on('enter kill phase', function(msg){
+        killPhase(msg.name, socket);
+    });
+
+    socket.on('enter save phase', function(msg){
+        savePhase(msg.name, socket);
+    });
+
+    socket.on('enter detect phase', function(msg){
+        detectPhase(msg.name, socket);
+    });
+
+    socket.on('reveal', function(msg){
+        const name = msg.name;
+        const role = msg.role;
+        if (role == 'mafia'){
+            $('#'+name+'_role').html(role);
+        } else {
+            $('#'+name+'_role').html('not mafia');
+        }
+    });
+
+    socket.on('reveal actual', function(msg){
+        const name = msg.name;
+        const role = msg.role;
+        $('#'+name+'_role').html(role);
+    });
+
+    socket.on('prevent start', function(){
+        document.getElementById("start").disabled = true;
+    });
+
+    socket.on('night over', function(msg){
+        const death = msg.deaths;
+        console.log(death);
+        if (death == ''){
+        } else {
+            $('#'+death+'_status').html('Status: Dead');
+        }
+        socket.emit('enable night');
+    })
+
+    socket.on('hang enable', function(){
+        $('a[name="hang"]').removeClass('disabled');
+        document.getElementById("night").disabled = false;
+    });
+
+    socket.on('death message', function(msg){
+        var display = count;
+        const display_string = display.toString();
+        const death = msg.deaths
+        if (death == ''){
+            addMessage('Night ' + display_string, 'All live to see another day..');
+            addWatcherMessage('Night ' + display_string, 'All live to see another day..');
+        } else {
+            addMessage('Night ' + display_string, death + ' was killed');
+            addWatcherMessage('Night ' + display_string, death + ' was killed');
+        }
+        socket.emit('increment', {night: count});
+    });
+
+    socket.on('add count', function(msg){
+        const track = msg.night;
+        count = track;
+    });
+
+    socket.on('result', function(msg){
+        const result = msg.winners;
+        console.log(result);
+        if (result == 'mafia'){
+            addMessage('Mafia Win', 'The Mafia have overthrown the townspeople, all who remain were executed');
+            addWatcherMessage('Mafia Win', 'The Mafia have overthrown the townspeople, all who remain were executed');
+            socket.emit('message');
+            $('#screenTitle').html('Mafia Win');
+            $('#screenSub').html('Everyone dies a horrible death under dictatorship rule');
+            localStorage.setItem('screen', $('#screen').html());
+            socket.emit('show screen');
+
+        } else {
+            addMessage('Village Win', 'The Mafia have been weeded out and hanged');
+            addWatcherMessage('Village Win', 'The Mafia have been weeded out and hanged');
+            socket.emit('message');
+            $('#screenTitle').html('Village Wins');
+            $('#screenSub').html('The Mafia were hanged and their families were enslaved for compensation of the dead villagers');
+            localStorage.setItem('screen', $('#screen').html());
+            socket.emit('show screen');
+        }
+        socket.emit('disable');
+        socket.emit('disable start');
+    });
+
+    socket.on('hang disable', function(){
+        $('a[name="hang"]').addClass('disabled');
+    });
+
+    $('#enter').click(function(){
+        var Name = $('#name').val();
+        var duplicate = false;
+        $('.card').each(function(){
+            if ($(this).attr("id") == Name) {
+                console.log($(this).attr("id"));
+                duplicate = true;
+            }
+        })
+        if (!duplicate){
+            appendToStorage('board', '<div class="card" id="' +Name+'" style="width: 18rem;">\
+                                        <img src="static/spyicon.png" class="card-img-top" alt="...">\
+                                        <div class="card-body">\
+                                            <h5 class="card-title">' + Name + '</h5>\
+                                            <p class="card-text" id="' + Name+ '_status">Status: Alive</p>\
+                                            <p class="card-text" id="' + Name+ '_role">???</p>\
+                                            <a name="kill" id="'+Name+'_kill" class="btn btn-primary disabled">Kill</a>\
+                                            <a name="save" id="'+Name+'_save" class="btn btn-secondary disabled">Save</a>\
+                                            <a name="detect" id="'+Name+'_detect" class="btn btn-secondary disabled">Detect</a>\
+                                            <a name="hang" id="'+Name+'_hang" class="btn btn-secondary disabled">Hang</a>\
+                                        </div>\
+                                    </div>'
+                                    );
+            $('#modal').modal('hide');
+            socket.emit('add player', {name: $('#name').val()});
+        } else {
+            $('.alert').show();
+        }
+    });
+
+    $(document).on("click", "a[name='kill']", function(){
+        const name = $(this).attr('id').split("_")[0];
+        addWatcherMessage('Kill Event', 'Mafia wants to kill ' + name);
+        socket.emit('watcher message');
+        socket.emit('kill check', {target: name});
+    });
+
+    $(document).on("click", "a[name='save']", function(){
+        const name = $(this).attr('id').split("_")[0];
+        addWatcherMessage('Save Event', 'Doctor wants to save ' + name);
+        socket.emit('watcher message');
+        socket.emit('save check', {target: name});
+    });
+
+    $(document).on("click", "a[name='detect']", function(){
+        const name = $(this).attr('id').split("_")[0];
+        const status = $('#'+name+'_status').html();
+        const role = $('#'+name+'_role').html();
+        socket.emit('detect check', {target: name});
+        addWatcherMessage('Detect Event', 'Detective wants to check ' + name);
+        socket.emit('watcher message');
+        if (status == 'Status: Alive' && role == '???'){
+            socket.emit('evaluate');
+        }
+    });
+
+    $(document).on("click", "a[name='hang']", function(){
+        const name = $(this).attr('id').split("_")[0];
+        const status = $('#'+name+'_status').html();
+        socket.emit('hang check', {target: name});
+        if (status == 'Status: Alive') {
+            socket.emit('hang', {target: name});
+            $('a[name="hang"]').addClass('disabled');
+            socket.emit('disable hang');
+        }
+    });
+
+    $('#observer').click(function() {
+        $('#modal').modal('hide');
+        socket.emit('observe');
+    })
+
+    $('#start').on('click', function() {
+        var log = localStorage.getItem('log');
+        var watcher_log = localStorage.getItem('watcher')
+        if (log == null) {
+            localStorage.setItem('log', $('#log').html());
+        }
+        if (watcher_log == null) {
+            localStorage.setItem('watcher', $('#log').html());
+        }
+        addMessage('Shuffling Roles', 'Randomly assign everyone a role');
+        addWatcherMessage('Shuffling Roles', 'Randomly assign everyone a role');
+        socket.emit('message');
+        socket.emit('shuffle');
+        addMessage('Start', 'Game started! The hunt begins..');
+        addWatcherMessage('Start', 'Game started! The hunt begins..');
+        socket.emit('message');
+        socket.emit('disable start');
+        nightProgression(socket);
+    });
+
+    $('#night').click(function() {
+        addMessage('A Night Begins!', 'Everyone goes to sleep..');
+        addWatcherMessage('A Night Begins!', 'Everyone goes to sleep..');
+        socket.emit('message');
+        nightProgression(socket);
+    });
+
+    $('#end').on('click', function(){
+        addMessage('Game Over', 'The game has ended');
+        socket.emit('message');
+        localStorage.clear();
+        socket.emit('clear');
+    });
+});
