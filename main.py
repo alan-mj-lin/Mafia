93e773b1e76668747dfaa2265052fed1fb21248e
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-05-01 23:08:09 -0400

    fixing self save check

--- main.py
+++ main.py
@@ -64,6 +64,8 @@ def self_save_check():
     print(current_save, prev_save)
     if prev_save == '':
         return True
+    elif prev_save != doctor and current_save != doctor:
+        return True
     elif prev_save == doctor and current_save == doctor:
         return False
 
